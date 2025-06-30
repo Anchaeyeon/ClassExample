b39b9d162bd7245ca02c5f91ea9e9ac127f4bbde
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-06-30 13:51:01 +0900

    메서드 오버라이딩 예제

--- Program.cs
+++ Program.cs
@@ -16,6 +16,10 @@ namespace ClassExample
             {
                 Console.WriteLine("부모의 메서드");
             }
+            public virtual void MethodO()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
             public void CountParent()
             {
                 Parent.counter++;
@@ -30,6 +34,10 @@ namespace ClassExample
         {
             public new string variable = "hiding";
             public new void Method()
+            {
+                Console.WriteLine("자식의 메서드");
+            }
+            public override void MethodO()
             {
                 Console.WriteLine("자식의 메서드");
             }
@@ -81,6 +89,10 @@ namespace ClassExample
             // 메서드 하이딩
             child3.Method(); // 자식의 메서드 출력
             ((Parent)child3).Method(); // 부모의 메서드 출력
+
+            // 메서드 오버라이딩
+            child3.MethodO(); // 자식의 메서드 출력
+            ((Parent)child3).MethodO(); // 부모의 메서드 출력
         }
     }
 }
